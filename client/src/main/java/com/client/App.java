7cd4e43838843901b7eb9b8036c134fa93e959d8
lai2-01 <lai2-01@itismeucci.com>
2022-12-12 12:46:34 +0100

    gestione login

--- client/src/main/java/com/client/App.java
+++ client/src/main/java/com/client/App.java
@@ -1,6 +1,5 @@
 package com.client;
 
-
 import java.io.IOException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,116 +11,91 @@ import java.util.regex.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class App 
-{
+public class App {
 
-    //questa variabile diventa true quando il server ha accettato la login (username valido per il server)
-    //sarà modificato dal ThreadClientInput alla ricezione del messaggio login ok
+    // questa variabile diventa true quando il server ha accettato la login
+    // (username valido per il server)
+    // sarà modificato dal ThreadClientInput alla ricezione del messaggio login ok
     // 0: non ancora inviato
     // 1: inviato e risposta non ancora ricevuta
     // 2: inviato e accettato
     // 3 inviato e non accettato
-    public static Interger userStatus = 0;
+    public static Integer userStatus = 0;
+    static boolean userConnected = false;
+    static Scanner keyboard = new Scanner(System.in);
 
+    static InputThreadClient inputClient;
 
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
-        Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         String userString;
-        
+
+        // creazione socket
         Socket socket = new Socket(InetAddress.getLocalHost(), 34567);
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        //DataInputStream  in = new DataInputStream(socket.getInputStream());
-        System.out.println("prima del input");
-        InputThreadClient inputClient = new InputThreadClient(socket);
+
+        // input dove arrivano le risposte del server
+        inputClient = new InputThreadClient(socket);
+
+        login();
+
+        // lo start gestisce i messaggi in arrivo, lo avviamo dopo aver fatto la login
         inputClient.start();
-        System.out.println("dopo input");
-    
-        while (true)
-        {//ciao 
-            /*
-             * 
-             * { 
-            “sendTo”: "*”;
-            "type":"command",
-            "text":"access", 
-            "userName": "dawid"   username da validare dal server
-}
-             */
-            
-            do {
-                login();
-                Thread.sleep(500);
-            } while (!userConnected);
-            
-            userString = "";
 
-            
-            if(userString.toUpperCase().equals("FINE"))
-            {
-                System.out.println("fine");
-                break;
-            }
 
-            else{
-                out.writeBytes(userString + '\n');
-                
-                
-            }
 
-            Message m1 = new Message("#","command","access","userName");
-
-            if(m1.getSendTo().equals("*"))
-            {
-                //messaggio rivolto a tutti
-
-            }
-            
-        }
+        //TODO gestire le scelte dell'utente e utilizzare inputClient.inviaMessaggio per inviare messaggi al server
         
-        socket.close();
-    }
-
-   private static void login() {
-        String userName = "";
-         while(true)
-        {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Inserisci userName da inviare al server" + '\n' + "numero caratteri minimo: 4" + '\n' + "NO SPAZI" +'\n');
-        userName = keyboard.next();
-        if (isValidUserName(userName))
-        {
-            System.out.println("userName inserito correttamente!");
-            break;
-        }
-        else
-        System.out.println("userName inserito errato");
-        }   
 
     }
 
-    private static void nameList()
-    {
-        ArrayList <String> nomi=new ArrayList <String>();
+    public static void login() {
 
+        do {
+            System.out.print("Inserisci userName da inviare al server" + '\n' + "numero caratteri minimo: 4" + '\n'
+                    + "NO SPAZI" + '\n');
+
+                    //leggo username da tastiera
+            String userName = keyboard.next();
+            if (isValidUserName(userName)) {
+                System.out.println("userName inserito correttamente!");
+
+                // messaggio da inviare per la login, ci metto dentro lo username scelto dall'utente
+                Message m = new Message();
+                m.setSendTo("#");
+                m.setType("command");
+                m.setTextString("access");
+                m.setUserName(userName);
+
+                inputClient.inviaMessaggio(m);
+
+                //risposta del server alla login
+                Messagge ricevuto = inputClient.riceviMessaggio();
+
+
+                //verificare la risposta del server
+
+                break;
+            } else {
+                System.out.println("userName inserito errato");
+            }
+        } while //fino a quando il server non dice ok(wilun mi deve dire quando è ok)
     }
 
+    
+
+    // controllo validità username (per il client)
     private static boolean isValidUserName(String userName) {
 
         Pattern pattern = Pattern.compile("[ ]", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(userName);
-        
-        if(userName.length()>4)
-        {
-            if (!matcher.find()) 
-            {
-                return true;
-            }
-            else 
-                return false;
-        }
-        else
-        return false;
-    }
-}
 
+        if (userName.length() > 4) {
+            if (!matcher.find()) {
+                return true;
+            } else
+                return false;
+        } else
+            return false;
+    }
+
+}
--- client/src/main/java/com/client/InputThreadClient.java
+++ client/src/main/java/com/client/InputThreadClient.java
@@ -8,38 +8,65 @@ import java.net.Socket;
 import java.net.http.WebSocket.Listener;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * L'istanza della 
+ * classe che gestisce il client
+ * ovvero ha il riferimento al socket e output/input stream
+ * 
+ * Il Thread gestisce i messaggi ricevuti dal server
+ * 
+ * 
+ * 
+ 
+ */
 public class InputThreadClient extends Thread {
 
-    private DataInputStream in;
     private Socket socket;
 
-    private String serverString = "";
+    private DataInputStream in;
+    private DataOutputStream out;
 
-    public InputThreadClient(Socket socket) {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public InputThreadClient(Socket socket) throws IOException {
         this.socket = socket;
-    }
-
-    public void run() {
-        try {
-            listenerS();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.out.println("ciao");
-        }
-    }
-
-    public void listenerS() throws IOException {
+        this.out = new DataOutputStream(socket.getOutputStream());
         this.in = new DataInputStream(socket.getInputStream());
-        while (true) {
-            serverString = in.readLine();
-            if (!serverString.equals("")) {
-                System.out.println("Risposta dal server: " + '\n' + serverString);
-            }
-
-        }
-
     }
+
+
+    public void inviaMessaggio(Message m) throws IOException {
+        // serializzo il messaggio in una stringa (Message -> String)
+        String msgDaInviare = mapper.writeValueAsString(m);
+        // invio il messaggio al server
+        out.writeBytes(msgDaInviare + "\n");
+    }
+
+    public Message riceviMessaggio() throws IOException {
+        // leggo il messaggio sotto forma di stringa dal server
+        String msgRicevuto = in.readLine();
+        // deserializzo il messaggio (String -> Message)
+        Message m = mapper.readValue(msgRicevuto, Message.class);
+        return m;
+    }
+
+     /**
+     * Thread: il thread viene avviato dopo la login
+     */
+    public void run() {
+
+        for (;;) {
+            try {
+                Message m = riceviMessaggio();
+
+                // in base al messaggio ricevuto decidi cosa fare
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
